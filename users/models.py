126906bdfe17fb3711be31a4dc77b4771ca15436
hann1010 <hanna.paivasto@gmail.com>
2021-09-11 21:27:17 +0300

    Added users

---
+++ users/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    club = models.CharField(max_length=200, blank=True)
+    call = models.CharField(max_length=200, blank=True)
+    qth = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+    town = models.CharField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    user_level= models.IntegerField(default=1)
+    list_rows= models.IntegerField(default=10)
+    
+    def __str__(self):
+        return  str(self.user) + " / " + self.club +" / " \
+        + self.call + " / " + self.qth + " / " \
+        + str(self.user_level)
+
+
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
