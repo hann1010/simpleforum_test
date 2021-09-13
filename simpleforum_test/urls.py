ffcf26efa2133ea69d122cf8db1ace45e1151e6a
hann1010 <hanna.paivasto@gmail.com>
2021-09-13 20:18:37 +0300

    Addings in urls,py

--- simpleforum_test/urls.py
+++ simpleforum_test/urls.py
@@ -14,8 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('',include('Lohkokirjanpito3App.urls')),
+    #path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    ##path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    #path('profile/', user_views.profile, name='profile'),
+    #path('password/',auth_views.PasswordChangeView.as_view(template_name='users/change-password.html',
+    #    success_url = '/'),name='change_password')
 ]
