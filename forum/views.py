2141bc668300a6841b935bafc82607147f0e9384
hann1010 <hanna.paivasto@gmail.com>
2021-09-16 20:34:52 +0300

    Added code view.py

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    dic_x = {
+        'title': 'Home'
+    }
+    return render(request, 'forum/index.html', dic_x)
