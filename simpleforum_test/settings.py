aaab073ea6950c5af5e03d64b9c6814adcad35e9
hann1010 <hanna.paivasto@gmail.com>
2021-09-17 22:50:21 +0300

    url home setup

--- simpleforum_test/settings.py
+++ simpleforum_test/settings.py
@@ -120,5 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'forum-home'
 LOGIN_URL = 'login'
