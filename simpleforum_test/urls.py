6125f53cd8c4f584a86df3ec22dfc6cd14757f34
hann1010 <hanna.paivasto@gmail.com>
2021-09-19 00:04:50 +0300

    Fix html

--- simpleforum_test/urls.py
+++ simpleforum_test/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('',include('forum.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    #path('profile/', user_views.profile, name='profile'),
+    path('profile/', user_views.profile, name='profile'),
     #path('password/',auth_views.PasswordChangeView.as_view(template_name='users/change-password.html',
     #    success_url = '/'),name='change_password')
 ]
