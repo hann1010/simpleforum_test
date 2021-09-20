51f5b1775fc937544fc576b60038a8b50e61f5c6
hann1010 <hanna.paivasto@gmail.com>
2021-09-20 20:02:39 +0300

    code to view

--- forum/views.py
+++ forum/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     dic_x = {
-        'title': 'Home'
+        'title': 'home'
     }
     return render(request, 'forum/index.html', dic_x)
+
+
+def latest_topic(request):
+    dic_x = {
+        'title': 'latest topic'
+    }
+    return render(request, 'forum/itemview.html', dic_x)
