8d7b9f08df07cdc08b7265b5953646c02fff32e5
hann1010 <hanna.paivasto@gmail.com>
2021-09-21 21:23:16 +0300

    added code to models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Forum_post(models.Model):
+    title = models.CharField(max_length=100, blank=True)
+    content = models.TextField(blank=True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    date_last_save = models.DateTimeField(auto_now=timezone.now)
+    post_id = models.AutoField(primary_key=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.title + " / " + str(self.author)
+
