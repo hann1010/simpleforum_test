765bca97b4dd4d2bcdb19f67afc4591dc798f95f
hann1010 <hanna.paivasto@gmail.com>
2021-09-22 19:43:06 +0300

    added field to models

--- forum/models.py
+++ forum/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Forum_post(models.Model):
     title = models.CharField(max_length=100, blank=True)
+    post_type = models.CharField(max_length=100, blank=True)
     content = models.TextField(blank=True)
     date_posted = models.DateTimeField(default=timezone.now)
     date_last_save = models.DateTimeField(auto_now=timezone.now)
