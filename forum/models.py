f0a8fd47acc146cf5ec6a84808a6d3fb939176d3
hann1010 <hanna.paivasto@gmail.com>
2021-09-24 18:51:52 +0300

    added CKEditor

--- forum/models.py
+++ forum/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Forum_post(models.Model):
     title = models.CharField(max_length=100, blank=True)
     post_type = models.CharField(max_length=100, blank=True)
     origin_post_id= models.IntegerField(default=0)
-    content = models.TextField(blank=True)
+    content = RichTextField()
     date_posted = models.DateTimeField(default=timezone.now)
     date_last_save = models.DateTimeField(auto_now=timezone.now)
     post_id = models.AutoField(primary_key=True)
