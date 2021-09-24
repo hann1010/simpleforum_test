ee71dbca99ff7e40e677376a1a873395ffd8f8bb
hann1010 <hanna.paivasto@gmail.com>
2021-09-24 20:19:33 +0300

    config CKEditor

--- simpleforum_test/settings.py
+++ simpleforum_test/settings.py
@@ -102,6 +102,20 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
 CKEDITOR_CONFIGS = {
         'default': {
             'toolbar': 'Custom',
@@ -116,20 +130,6 @@ CKEDITOR_CONFIGS = {
             ]
         }
     }
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
