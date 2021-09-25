a8b027643214d3cc3520e6077a8b7ba158a0cd42
hann1010 <hanna.paivasto@gmail.com>
2021-09-25 21:42:03 +0300

    create TopicCreateView

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,16 @@
 from . import views
 from django.urls import path
+from .views import (
+    #PostListView,
+    #PostDetailView,
+    TopicCreateView,
+    #PostUpdateView,
+    #PostDeleteView
+)
 
 urlpatterns = [
 	path('', views.home, name='forum-home'),
 	path('latest/topic/', views.latest_topic, name='forum-latest_topic'),
+	path('topic/new/', TopicCreateView.as_view(), name='forum_topic-create'),
 	
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from .models import Forum_post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    #ListView,
+    #DetailView,
+    CreateView,
+    #UpdateView,
+    #DeleteView,
+)
 
 def home(request):
     dic_x = {
@@ -16,3 +24,20 @@ def latest_topic(request):
             'posts' : Forum_post.objects.all().order_by('-date_posted')
         }
     return render(request, 'forum/itemview.html', dic_x)
+
+
+class TopicCreateView(LoginRequiredMixin, CreateView):
+    model = Forum_post
+    success_url = '/'
+    fields = ['title', 'content']
+
+    def get_template_names(self):
+        if  self.request.user.profile.user_level > 3:
+            template_name = 'forum/topic_new.html'
+        else:
+            template_name = ''
+        return template_name
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
