e75763fb1abf91d5e0ca428fdc6c55aadd25b654
hann1010 <hanna.paivasto@gmail.com>
2021-10-01 19:14:36 +0300

    fix urls

--- forum/urls.py
+++ forum/urls.py
@@ -12,11 +12,11 @@ from .views import (
 from . import views
 
 urlpatterns = [
-	path('', views.home, name='forum-home'),
-	path('latest/topics/', views.latest_topics, name='forum-latest_topics'),
+    path('', views.home, name='forum-home'),
+    path('latest/topics/', views.latest_topics, name='forum-latest_topics'),
     path('latest/comments/', views.latest_comments, name='forum-latest_comments'),
     path('latest/all/', views.latest_all, name='forum-latest_all'),
     path('topic/new/', TopicCreateView.as_view(), name='forum_topic-create'),
-	path('all/<int:pk>/comment/new/', CommentCreateView.as_view(), name='forum_comment-create'),
+    path('all/<int:pk>/comment/new/', CommentCreateView.as_view(), name='forum_comment-create'),
 	
 ]
