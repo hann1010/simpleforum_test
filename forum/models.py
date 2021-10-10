5a5bd75d0fa4219faff19bc66c627e75a91a12f7
hann1010 <hanna.paivasto@gmail.com>
2021-10-10 17:57:27 +0300

    fixed models

--- forum/models.py
+++ forum/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 class Forum_post(models.Model):
-    title = models.CharField(max_length=100, blank=True)
+    title = models.CharField(max_length=100, blank=False)
     post_type = models.CharField(max_length=100, blank=True)
     origin_post_id= models.IntegerField(default=0)
     content = RichTextField()
