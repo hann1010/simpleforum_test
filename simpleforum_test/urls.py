5bbeaf106b5297462b7afba32c6890030bd08c1e
hann1010 <hanna.paivasto@gmail.com>
2021-10-10 19:32:34 +0300

    added password_reset 3/4

--- simpleforum_test/urls.py
+++ simpleforum_test/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('profile/', user_views.profile, name='profile'),
-    path('password/',auth_views.PasswordChangeView.as_view(template_name='users/change-password.html',
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='users/change-password.html',
         success_url = '/'),name='change_password'),
     path('password-reset/',
          auth_views.PasswordResetView.as_view(
@@ -35,6 +35,9 @@ urlpatterns = [
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(
              template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+             template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+
 ] 
 
 
