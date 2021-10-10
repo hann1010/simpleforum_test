5936a04521aa1193c67b5b364dd765882df982ca
hann1010 <hanna.paivasto@gmail.com>
2021-10-10 20:27:48 +0300

    setup EMAIL_BACKEND for develoment

--- simpleforum_test/settings.py
+++ simpleforum_test/settings.py
@@ -147,8 +147,8 @@ MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL = 'forum-home'
 LOGIN_URL = 'login'
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #develoment only
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_HOST = 'smtp.gmail.com'
 #EMAIL_PORT = 587
 #EMAIL_USE_TLS = True
