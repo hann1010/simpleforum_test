8b941a82269f699679a3e129fbc08308ff8fed1e
hann1010 <hanna.paivasto@gmail.com>
2021-11-13 20:48:52 +0200

    fixing index

--- forum/forms.py
+++ forum/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class FilterForm(forms.Form):
     title_filter = forms.CharField(max_length=100, required=False,  
-        widget=forms.TextInput( attrs={'placeholder': ' Filter..', 'class': 'form-control'}))
+        widget=forms.TextInput( attrs={'placeholder': ' Filtering from titles..', 'class': 'form-control'}))
