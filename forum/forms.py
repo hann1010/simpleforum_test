71af52c2a3775fcac9e006f65b201ec360c55179
hann1010 <hanna.paivasto@gmail.com>
2021-12-04 21:31:10 +0200

    code to user info

--- forum/forms.py
+++ forum/forms.py
@@ -3,3 +3,4 @@ from django import forms
 class FilterForm(forms.Form):
     title_filter = forms.CharField(max_length=100, required=False,  
         widget=forms.TextInput( attrs={'placeholder': 'Filtering from titles..', 'class': 'form-control'}))
+
