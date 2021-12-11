e170ee2f922220e7e5a4633ab0026816da84037a
hann1010 <hanna.paivasto@gmail.com>
2021-12-11 20:59:00 +0200

    added Pillow for profile pic

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -21,6 +22,16 @@ class Profile(models.Model):
         return  str(self.user) + " / " + self.club +" / " \
         + self.call + " / " + self.qth + " / " \
         + str(self.user_level)
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 @receiver(post_save, sender=User)
