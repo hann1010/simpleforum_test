0b39f9e7f13d8483b959365c2f48a4dd320fcdec
hann1010 <hanna.paivasto@gmail.com>
2022-01-02 21:32:20 +0200

    fixed registration bug (Error500)

--- users/models.py
+++ users/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from PIL import Image
 
 
@@ -34,11 +32,6 @@ class Profile(models.Model):
             img.save(self.image.path)
 
 
-@receiver(post_save, sender=User)
-def update_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-    instance.profile.save()
 
 
 
